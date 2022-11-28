14286da47b70c01524ece78ece875b13a0612af9
jim w <jimscarver#gmail.com>
2022-11-28 15:40:42 -0500

    read markdown file

--- src/md2html.js
+++ src/md2html.js
@@ -1,8 +1,12 @@
 const gfm = require('cmark-gfm-js');
 
-const markdown = '# Hi\nThis ~text~~~~ is ~~~~curious 😡🙉🙈~.';
-let html = gfm.convert(markdown);
-console.log(html);
+let filename = process.argv[1]
+console.log(filename)
+fs = require('fs')
+fs.readFile('pages/'+filename+'.md', 'utf8', function (err,data) {
+ const markdown = data
+ let html = gfm.convert(markdown);
+ console.log(html);
 /** Prints: 
   <h1>Hi</h1>
   <p>This <del>text</del> is <del>curious 😡🙉🙈</del>.</p>
@@ -15,3 +19,4 @@ console.log(html);
   <h1 data-sourcepos="1:1-1:4">Hi</h1>
   <p data-sourcepos="2:1-2:44">This <del>text</del> is <del>curious 😡🙉🙈</del>.</p>
 */
+})
