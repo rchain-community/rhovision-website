a55a85d52b1ca269a980e54a7fdd0823cbb7c9d5
jim w <jimscarver#gmail.com>
2022-12-02 19:24:53 -0500

    a start

--- src/md2html.js
+++ src/md2html.js
@@ -1,22 +1,9 @@
-const gfm = require('cmark-gfm-js');
-
-let filename = process.argv[1]
-console.log(filename)
+// usage: node src/md2html.js pages/home.md >html/home.html
+let filename = process.argv[2]
 fs = require('fs')
-fs.readFile('pages/'+filename+'.md', 'utf8', function (err,data) {
- const markdown = data
- let html = gfm.convert(markdown);
- console.log(html);
-/** Prints: 
-  <h1>Hi</h1>
-  <p>This <del>text</del> is <del>curious 😡🙉🙈</del>.</p>
-*/
-
-// Specify an option
-html = gfm.convert(markdown, gfm.Option.sourcePos);
-console.log(html);
-/** Prints
-  <h1 data-sourcepos="1:1-1:4">Hi</h1>
-  <p data-sourcepos="2:1-2:44">This <del>text</del> is <del>curious 😡🙉🙈</del>.</p>
-*/
-})
+data = fs.readFileSync(filename, 'utf8')
+const markdown = data
+// console.log(data);
+const gfm = require('cmark-gfm-js');
+let html = gfm.convert(markdown);
+console.log(html)
