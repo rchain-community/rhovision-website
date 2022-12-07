4c39e6400a388dcd820887d58fcbc080e7e7cc41
jim w <jimscarver#gmail.com>
2022-12-07 16:00:18 -0500

    simple server

---
+++ web.js
@@ -0,0 +1,23 @@
+const http = require('http');
+var url = require('url');
+// var querystring = require('querystring');
+const fs = require('fs');
+
+const requestListener = function (req, res) {
+	const queryObject = url.parse(req.url, true).query;
+    //console.log(req.url);
+    page = queryObject.page ? queryObject.page : "home";;
+    console.log(page);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('html/'+page+'.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    return;
+    }
+    // :console.log(data);
+    res.end(data);
+});
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
